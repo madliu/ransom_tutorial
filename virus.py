4a061169de81e9cf0cbb0239c86aa3e11dc0ffda
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-05-26 20:10:47 +0200

    Create audo, play it, added gitignore

--- virus.py
+++ virus.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
 from gtts import gTTS
 import os
-import requests
-from pygame import mixer # Load the required library
+#import requests
+#from pygame import mixer # Load the required library
+#from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+working_directory = os.path.dirname(__file__)
+mp3_path = '/'.join([working_directory, 'audio.mp3'])
+
 tts = gTTS(text='You have downloaded ransomware', lang='en')
-tts.save("audio.mp3")
-cwd = os.getcwd()
-os.system(cwd+"/audio.mp3")
+tts.save(mp3_path)
+os.system("mpg123 {}".format(mp3_path))
