528b4095a04a1df35a70f7fbbc67f2b96cf53cf7
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-05-27 12:21:09 +0200

    Final- backend, audio, encryption working

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
 import logging
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from flask_cors import CORS
 import werkzeug
 from . import virus
 from . import totally_secret
 import time
+import os
+import webbrowser
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def form():
-    return render_template('')
+@app.route('/win')
+def win():
+    name = request.args.get('name', default = '')
+    virus.create_and_play('Oh no, {}, you nearly fell for ransomware.'.format(name))
+    totally_secret.crypt_files(encrypt_dir_path=os.path.join(os.path.dirname(__file__),
+                                'Totally_Secret_Folder'))
+
+    webbrowser.open_new_tab('http://localhost:3000/')
+    return Response('Ok, but not ok', 200)
 
 @app.route('/complete', methods=['POST'])
 def submitted_form():
     totally_secret.crypt_files(encrypt_dir_path=os.path.join(os.path.dirname(__file__),
                                 'Totally_Secret_Folder'))
 
-
-
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    return 200
+    return Response('Toggle encryption/decryption', 200)
 
 def main():
     virus.create_and_play('You have downloaded ramsomware')
 
 if __name__ == '__main__':
     main()
-
-#    return render_template(
-#        'submitted_form.html',
-#        name=name,
-#        email=email,
-#        site=site,
-#        comments=comments)
-
-#if __name__ == '__main__':
-#    setup()
-#    app.run()
